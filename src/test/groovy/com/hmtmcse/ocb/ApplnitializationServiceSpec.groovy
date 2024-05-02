b8e59f65c1d42b50229dbd3c1297e50a5e7ff914
Davi-Cesar <davicesar2515@gmail.com>
2024-05-01 22:10:59 -0300

    Revert "Revert "feat: parte 6 do tutorial""
    This reverts commit be9c0fd97b26349458e16867ab623040a25ea80c.

---
+++ src/test/groovy/com/hmtmcse/ocb/ApplnitializationServiceSpec.groovy
@@ -0,0 +1,18 @@
+package com.hmtmcse.ocb
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class ApplnitializationServiceSpec extends Specification implements ServiceUnitTest<ApplnitializationService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
